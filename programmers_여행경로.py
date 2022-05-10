a85b9797f6481dafca3f293f21332e0d516a1f92
wonyounglee <lwy970327@naver.com>
2022-05-10 12:48:32 +0900

    재시도 필요

---
+++ programmers_여행경로.py
@@ -0,0 +1,22 @@
+def solution(tickets):
+    answer = ['ICN']
+    for_stack = []
+    for i in tickets:
+        if 'ICN' == i[0]:
+            for_stack.append(i)
+    for_stack.sort()
+    stack = [for_stack[0]]
+    tickets.remove(for_stack[0])  # 여기까지는 'icn'만 stack에 넣은 경우임
+    
+    while len(stack)>0:
+        pop_value = stack.pop()
+        answer.append(pop_value[1])
+        for_stack = []
+        for i in tickets:
+            if pop_value[1] == i[0]:
+                for_stack.append(i) # for_stack 을 만들고 첫번쨰만 꺼낼 예정 
+        for_stack.sort()
+        stack.append(for_stack[0])
+        tickets.remove(for_stack[0])
+    return answer
+solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]])
