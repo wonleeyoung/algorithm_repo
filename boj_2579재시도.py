c35fd82bc1ffcc05c7ce257af299da3b687d0f03
wonyounglee <lwy970327@naver.com>
2022-05-17 20:08:35 +0900

    프로그래머스 정수삼각형

---
+++ boj_2579재시도.py
@@ -0,0 +1,7 @@
+n = int(input())
+score_list = []
+for i in range(n):
+    a= int(input())
+    score_list.append(a)
+list1 = [[0,-1,0]]
+print(score_list)
---
+++ prgrammers_정수삼각형.py
@@ -0,0 +1,16 @@
+def solution(triangle):
+    answer = 0
+    for i in range(len(triangle)):
+        for j in range(len(triangle[i])):
+            if i==0:
+                pass
+            elif j == 0:
+                triangle[i][j] += triangle[i-1][0]
+            elif j == len(triangle[i])-1:
+                triangle[i][j] += triangle[i-1][j-1]
+            else :
+                triangle[i][j] = max(triangle[i-1][j-1]+triangle[i][j],triangle[i-1][j]+triangle[i][j])
+    a= triangle[-1]
+    a.sort()
+    answer = a[-1]
+    return answer
