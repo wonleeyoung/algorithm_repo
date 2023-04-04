b1f7cfd79797a8094bd5206e1e667d6267e8df14
wonyounglee <lwy970327@naver.com>
2023-04-04 09:27:57 +0900

    구현 마무리

---
+++ 구현/main.py
@@ -0,0 +1,26 @@
+from typing import List
+# Skeleton code for even_list
+def even_list(int_list: List[int]) -> List[int]:
+    newlist = []
+    for i in int_list:
+        if i%2 == 0:
+            newlist.append(i)
+    return newlist
+
+
+def sum_of_squares_of_even(even_int_list : List[int]) -> int:
+    sum = 0
+    for i in even_int_list:
+        sum = sum + i*i
+    return sum
+
+
+def main():
+    # Example list
+    int_list = [1,2,3,4,5,6,7,8,9,10]
+    even_int_list = even_list(int_list)
+    output = sum_of_squares_of_even(even_int_list)
+    print(output)
+    
+if __name__ == "__main__":
+    main()
