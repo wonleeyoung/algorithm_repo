c5aebfb83cec583be1efec3beb70ee23f8d47230
wonyounglee <lwy970327@naver.com>
2023-04-06 11:16:36 +0900

    개미 전사

---
+++ DP/연습2.py
@@ -0,0 +1,12 @@
+n = int(input())
+store = list(map(int, input().split()))
+
+dp = [0 for i in range(n)]
+
+dp[0] = store[0]
+dp[1] = store[1]
+dp[2] = store[0] + store[2]
+
+for i in range(3,n):
+    dp[i] = max(dp[i-3]+store[i],dp[i-2]+store[i])
+print(dp[n-1])
